97aa53b52797e697cd1d4d0ab55ea625089f5bdf
gen-e-sis <67941300+gen-e-sis@users.noreply.github.com>
2023-11-28 08:17:18 +0800

    Update CISQueue.java

--- src/CISQueue.java
+++ src/CISQueue.java
@@ -1,22 +1,71 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class CISQueue {
 
-    // Linked list property.
+    // Array property.
+    public LinkedList<Integer> linkedList;
 
     // Size property.
+    private int size;
+
 
     // Constructor.
+    public CISQueue()
+    {
+        linkedList = new LinkedList<>();;
+        size = 0;
+    }
 
-    // Enqueue. This method adds a node to the end of the linked list.
+    // Push. This method pushes a value onto the top of the stack.
+    public void enqueue (int element)
+    {
+        linkedList.add(element);
+        size++;
+    }
 
-    // Dequeue. This method removes a node from the beginning of the linked list.
+    // Pop. This method pops a value from the top of the stack.
+    public Object dequeue()
+    {
+        int temp = 0;
+        if (size == 0)
+        {
+            return null;
+        }
+        size--;
+        temp = linkedList.getFirst();
+        linkedList.removeFirst();
+        return temp;
+    }
 
     // isEmpty. Returns a boolean indicating whether the linked list is empty.
+    public boolean isEmpty()
+    {
+        if (size == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
     // size. Returns the size of the queue.
+    public int size()
+    {
+        return size;
+    }
 
     // toString. Returns a description of the queue in, for example, the following format:
-    // CISQueue{queue=[7, 11], size=2}
+    // CISStack{stack=[7, 11], size=2, pointer=1}
 
+
+    @Override
+    public String toString() {
+        return "CISQueue{" +
+                "queue=" + linkedList +
+                ", size=" + size +
+                '}';
+    }
 }
